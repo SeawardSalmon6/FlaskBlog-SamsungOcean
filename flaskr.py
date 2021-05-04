8bbe88af46375d8419c4758b27ec6cf64f8d5642
jean <rayhanachour@hotmail.com>
2021-05-04 00:04:20 -0300

    uploading last and updated files from this project

--- flaskr.py
+++ flaskr.py
@@ -2,10 +2,11 @@ import sqlite3
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
-DATABASE = "./tmp/flaskr.db"
+DATABASE = "tmp/flaskr.db"
 SECRET_KEY = "teste"
 USERNAME = "admin"
 PASSWORD = "default"
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 def criar_bd():
